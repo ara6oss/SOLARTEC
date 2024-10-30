310618209c561568964c47c1fc973e90c01c7744
ara4matic <armangaziz123@mail.ru>
2024-10-30 12:37:28 +0500

    added: OOP pattern

--- Solar/GreenApi/urls.py
+++ Solar/GreenApi/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
 
    path('sendMessage', sendMessageView.as_view(), name='send_message'),
    path('sendFile', sendMultimediaView.as_view(), name='send_file'),
-   path('sendLocation', SendLocationView.as_view(), name='send_location'),
-   path('getNotification', getNotification.as_view(), name='get_notification'),
+   path('sendLocation', sendLocationView.as_view(), name='send_location'),
+   path('getNotification', getNotificationView.as_view(), name='get_notification'),
    path('selenium', selenium.as_view(), name='selenium'),
 
 ]
--- Solar/GreenApi/views.py
+++ Solar/GreenApi/views.py
@@ -1,28 +1,66 @@
 import json
 import os
+from django.conf import settings
 import requests
 from django.http import JsonResponse
 from django.views import View
-from django.conf import settings
 from dotenv import load_dotenv
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 from GreenApi.models import Client, Notification
 
 # Load environment variables
 load_dotenv()
 
+
+class GreenAPIService:
+    def __init__(self):
+        self.api_url = os.getenv('API_URL')
+        self.id_instance = os.getenv('ID_INSTANCE')
+        self.api_token_instance = os.getenv('API_TOKEN_INSTANCE')
+
+    def get_url(self, endpoint):
+        return f"{self.api_url}/waInstance{self.id_instance}/{endpoint}/{self.api_token_instance}"
+
+    def send_request(self, url, payload=None, files=None, method='post'):
+        headers = {'Content-Type': 'application/json'}
+        try:
+            if method == 'post':
+                response = requests.post(url, json=payload, files=files, headers=headers)
+            else:
+                response = requests.get(url, headers=headers)
+            return response, None
+        except requests.exceptions.RequestException as e:
+            return None, str(e)
+
+    def create_client_if_not_exists(self, chat_id):
+        try:
+            client = Client.objects.get(phone_number=chat_id)
+        except Client.DoesNotExist:
+            client = Client.objects.create(phone_number=chat_id, name="Unknown")
+        return client
+
+    def create_notification(self, client, message_type, direction, content, status):
+        Notification.objects.create(
+            client=client,
+            message_type=message_type,
+            direction=direction,
+            content=content,
+            status=status
+        )
+
+
 class sendMessageView(View):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.green_api_service = GreenAPIService()
+
     def post(self, request, *args, **kwargs):
         try:
-            # Получение данных из JSON запроса
             request_data = json.loads(request.body)
             chat_id = request_data.get('chatId')
             message = request_data.get('message')
@@ -31,51 +69,25 @@ class sendMessageView(View):
 
         if not chat_id or not message:
             return JsonResponse({'error': 'Phone number and message are required.'}, status=400)
-        
 
-        # Получение конфиденциальных данных из .env
-        api_url = os.getenv('API_URL')
-        id_instance = os.getenv('ID_INSTANCE')
-        api_token_instance = os.getenv('API_TOKEN_INSTANCE')
+        send_message_url = self.green_api_service.get_url('sendMessage')
+        payload = {"chatId": f"{chat_id}@c.us", "message": message}
+
+        response, error = self.green_api_service.send_request(send_message_url, payload=payload)
+        if error:
+            return JsonResponse({'error': 'An error occurred while sending the request.', 'details': error}, status=500)
+
+        response_data = response.json()
+        if response.status_code == 200:
+            client = self.green_api_service.create_client_if_not_exists(chat_id)
+            self.green_api_service.create_notification(client, 'text', 'outgoing', message, 'sent')
+            return JsonResponse({'success': 'Message sent successfully.', 'data': response_data}, status=200)
+        else:
+            return JsonResponse({'error': 'Failed to send message.', 'details': response_data}, status=response.status_code)
 
 
-        # Формирование URL для отправки сообщения
-        send_message_url = f"{api_url}/waInstance{id_instance}/sendMessage/{api_token_instance}"
-
-        # Создание данных для отправки запроса к Green API
-        payload = {
-            "chatId": f"{chat_id}@c.us",
-            "message": message
-        }
-
-        # Отправка POST-запроса к Green API
-        try:
-            response = requests.post(send_message_url, json=payload)
-            response_data = response.json()
-
-            if response.status_code == 200:
-                try:
-                    client = Client.objects.get(phone_number=chat_id)
-                except Client.DoesNotExist:
-                    client = Client.objects.create(phone_number=chat_id, name="Unknown")
-
-                Notification.objects.create(
-                client=client,
-                message_type='text',
-                direction='outgoing',
-                content=message,
-                status='sent'
-        )
-                
-                return JsonResponse({'success': 'Message sent successfully.', 'data': response_data}, status=200)
-            else:
-                return JsonResponse({'error': 'Failed to send message.', 'details': response_data}, status=response.status_code)
-        except requests.exceptions.RequestException as e:
-            return JsonResponse({'error': 'An error occurred while sending the request.', 'details': str(e)}, status=500)
-        
 class sendMultimediaView(View):
     def post(self, request, *args, **kwargs):
-        # Получение данных из запроса
         file = request.FILES.get('file')
         chat_id = request.POST.get('chatId')
         caption = request.POST.get('caption', '')
@@ -83,19 +95,14 @@ class sendMultimediaView(View):
         if not chat_id or not file:
             return JsonResponse({'error': 'Phone number, file, and media type are required.'}, status=400)
 
-        # Сохранение файла временно
         file_path = default_storage.save(file.name, ContentFile(file.read()))
         full_file_path = os.path.join(settings.MEDIA_ROOT, file_path)
 
-        # Получение конфиденциальных данных из .env
         api_url = os.getenv('API_URL')
         id_instance = os.getenv('ID_INSTANCE')
         api_token_instance = os.getenv('API_TOKEN_INSTANCE')
-
-        # Формирование URL для отправки файла
         send_file_url = f"{api_url}/waInstance{id_instance}/sendFileByUpload/{api_token_instance}"
 
-        # Создание данных для отправки запроса к Green API
         payload = {
             'chatId': f"{chat_id}@c.us",
             'caption': caption
@@ -103,13 +110,10 @@ class sendMultimediaView(View):
         files = {
             'file': (file.name, open(full_file_path, 'rb'), file.content_type)
         }
-        
-        # Отправка POST-запроса к Green API
+
         try:
             response = requests.post(send_file_url, data=payload, files=files)
             response_data = response.json()
-
-            # Удаление временного файла
             default_storage.delete(file_path)
 
             if response.status_code == 200:
@@ -119,81 +123,80 @@ class sendMultimediaView(View):
                     client = Client.objects.create(phone_number=chat_id, name="Unknown")
 
                 Notification.objects.create(
-                client=client,
-                message_type='file',
-                direction='outgoing',
-                content=[chat_id, files],
-                status='sent'
-        )
+                    client=client,
+                    message_type='file',
+                    direction='outgoing',
+                    content=[chat_id, files],
+                    status='sent'
+                )
                 return JsonResponse({'success': 'File sent successfully.', 'data': response_data}, status=200)
             else:
                 return JsonResponse({'error': 'Failed to send file.', 'details': response_data}, status=response.status_code)
         except requests.exceptions.RequestException as e:
-            # Удаление временного файла в случае ошибки
             default_storage.delete(file_path)
             return JsonResponse({'error': 'An error occurred while sending the request.', 'details': str(e)}, status=500)
 
 
 
-class SendLocationView(View):
+class sendLocationView(View):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.green_api_service = GreenAPIService()
+
     def post(self, request, *args, **kwargs):
         try:
-            # Получение данных из JSON-запроса
             data = json.loads(request.body)
             chat_id = data.get('chatId')
             name_location = data.get('nameLocation')
             address = data.get('address')
             latitude = data.get('latitude')
             longitude = data.get('longitude')
-
-            # Проверка наличия обязательных параметров
-            if not all([chat_id, name_location, address, latitude, longitude]):
-                return JsonResponse({'error': 'All fields are required.'}, status=400)
-
-            # Получение конфиденциальных данных из .env
-            api_url = os.getenv('API_URL')
-            id_instance = os.getenv('ID_INSTANCE')
-            api_token_instance = os.getenv('API_TOKEN_INSTANCE')
-
-            # Формирование URL для отправки местоположения
-            send_location_url = f"{api_url}/waInstance{id_instance}/sendLocation/{api_token_instance}"
-
-            # Создание данных для отправки запроса к Green API
-            payload = {
-                "chatId": f"{chat_id}@c.us",
-                "nameLocation": name_location,
-                "address": address,
-                "latitude": latitude,
-                "longitude": longitude
-            }
-
-            headers = {
-                'Content-Type': 'application/json'
-            }
-
-            response = requests.post(send_location_url, json=payload, headers=headers)
-            response_data = response.json()
-
-            if response.status_code == 200:
-                try:
-                    client = Client.objects.get(phone_number=chat_id)
-                except Client.DoesNotExist:
-                    client = Client.objects.create(phone_number=chat_id, name="Unknown")
-
-                Notification.objects.create(
-                client=client,
-                message_type='location',
-                direction='outgoing',
-                content=[name_location, address, latitude, longitude],
-                status='sent'
-        )
-                return JsonResponse({'success': 'Location sent successfully.', 'data': response_data}, status=200)
-            else:
-                return JsonResponse({'error': 'Failed to send location.', 'details': response_data}, status=response.status_code)
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON payload.'}, status=400)
-        except requests.exceptions.RequestException as e:
-            return JsonResponse({'error': 'An error occurred while sending the request.', 'details': str(e)}, status=500)
+
+        if not all([chat_id, name_location, address, latitude, longitude]):
+            return JsonResponse({'error': 'All fields are required.'}, status=400)
+
+        send_location_url = self.green_api_service.get_url('sendLocation')
+        payload = {
+            "chatId": f"{chat_id}@c.us",
+            "nameLocation": name_location,
+            "address": address,
+            "latitude": latitude,
+            "longitude": longitude
+        }
+
+        response, error = self.green_api_service.send_request(send_location_url, payload=payload)
+        if error:
+            return JsonResponse({'error': 'An error occurred while sending the request.', 'details': error}, status=500)
+
+        response_data = response.json()
+        if response.status_code == 200:
+            client = self.green_api_service.create_client_if_not_exists(chat_id)
+            self.green_api_service.create_notification(client, 'location', 'outgoing', [name_location, address, latitude, longitude], 'sent')
+            return JsonResponse({'success': 'Location sent successfully.', 'data': response_data}, status=200)
+        else:
+            return JsonResponse({'error': 'Failed to send location.', 'details': response_data}, status=response.status_code)
+
+
+class getNotificationView(View):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.green_api_service = GreenAPIService()
+
+    def get(self, request):
+        receive_timeout = request.GET.get('receiveTimeout', 10)
+        get_notification_url = self.green_api_service.get_url('receiveNotification') + f"?receiveTimeout={receive_timeout}"
+
+        response, error = self.green_api_service.send_request(get_notification_url, method='get')
+        if error:
+            return JsonResponse({'error': 'An error occurred while retrieving the notification.', 'details': error}, status=500)
+
+        response_data = response.json()
+        if response.status_code == 200:
+            return JsonResponse({'success': 'Notification retrieved successfully.', 'data': response_data}, status=200)
+        else:
+            return JsonResponse({'error': 'Failed to retrieve notification.', 'details': response_data}, status=response.status_code)
         
 
 class getNotification(View):
